7c343d38afbebef85c318ca6ff99d15c40cd31db
Ahmed Awad ( Nullc0d3 ) <ahmed.m.ameen@gmail.com>
2026-07-21 00:19:16 +0300

    add Google Tag Manager (GTM-TBHL394X) with noscript fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TBHL394X');`,
+        }} />
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -96,6 +99,7 @@ var p=document.getElementById('cv-print-btn');if(p)p.addEventListener('click',fu
         <JsonLd />
       </head>
       <body className={`${inter.className} antialiased`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBHL394X" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[60] focus:px-4 focus:py-2 focus:rounded-lg focus:bg-primary-600 focus:text-white focus:font-medium focus:shadow-lg">
           Skip to main content
         </a>
